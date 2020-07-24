7df5b22e02282261788d56167da85dd67f215298
Vadim <vadimpishprivate@gmail.com>
2020-07-24 14:45:24 -0700

    working wp-json integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './elements/header/header.component';
@@ -30,7 +32,8 @@ import { ParamsService } from './projects/shared/params.service';
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     ParamsService
--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -1,28 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../shared/project.service';
 import { Project } from '../shared/project.model';
+import { Observable } from 'rxjs';
 import { ParamsService } from '../shared/params.service';
+
 
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
-  styleUrls: ['./project-list.component.scss'],
-  providers: [ProjectService]
+  styleUrls: ['./project-list.component.scss']
 })
 export class ProjectListComponent implements OnInit {
+  
+  projects: Observable<any[]>;
 
-  projects: Project [] = [];
-
-  sliceAmount:number; 
+  sliceAmount: number;
 
   constructor(private projectService: ProjectService,
-              private paramsService: ParamsService) { }
+    private paramsService: ParamsService) {
+
+  }
 
   ngOnInit(): void {
+    this.getPosts();
     this.paramsService.currentMessage.subscribe(sliceAmount => this.sliceAmount = sliceAmount);
-
-    this.projects = this.projectService.projects;
+    // this.projects = this.projectService.projects;
   }
+
+  public getPosts(){
+    this.projectService.getAllProjects().subscribe((data:  Observable<any>) => {
+        this.projects = data;
+        console.log(data);
+    });
+}
 
 }
