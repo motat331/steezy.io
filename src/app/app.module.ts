9e940142dbc465f7c6aff831fcfe0a159cbca000
Vadim <vadimpishprivate@gmail.com>
2021-02-16 19:19:48 -0800

    added preboot for heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER, PLATFORM_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -15,6 +15,8 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { ContactFormComponent } from './elements/contact-form/contact-form.component';
 import { ProjectListComponent } from './projects/project-list/project-list.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { PrebootModule } from 'preboot';
+import { isPlatformBrowser, ɵgetDOM, DOCUMENT } from '@angular/common';
 
 @NgModule({
     declarations: [
@@ -31,13 +33,46 @@ import { ProjectsComponent } from './projects/projects.component';
     ],
     imports: [
         BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        PrebootModule.withConfig({ appRoot: 'app-root' }),
         FontAwesomeModule,
         AppRoutingModule,
         HttpClientModule,
         FormsModule,
         SlickCarouselModule,
     ],
-    providers: [],
+    providers: [
+        {
+            provide: APP_INITIALIZER,
+            useFactory: function (
+                document: HTMLDocument,
+                platformId: Object
+            ): Function {
+                return () => {
+                    if (isPlatformBrowser(platformId)) {
+                        const dom = ɵgetDOM();
+                        const styles: any[] = Array.prototype.slice.apply(
+                            dom
+                                .getDefaultDocument()
+                                .querySelectorAll(`style[ng-transition]`)
+                        );
+                        styles.forEach((el) => {
+                            // Remove ng-transition attribute to prevent Angular appInitializerFactory
+                            // to remove server styles before preboot complete
+                            el.removeAttribute('ng-transition');
+                        });
+                        document.addEventListener('PrebootComplete', () => {
+                            // After preboot complete, remove the server scripts
+                            setTimeout(() =>
+                                styles.forEach((el) => dom.remove(el))
+                            );
+                        });
+                    }
+                };
+            },
+            deps: [DOCUMENT, PLATFORM_ID],
+            multi: true,
+        },
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
