e96cb4bd9248cc7439f01408ae6827407b127378
Vadim <vadimpishprivate@gmail.com>
2021-08-29 18:48:43 -0700

    removed dark mode for now

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -26,33 +26,30 @@ export class HeaderComponent implements OnInit {
 
     darkModeInit() {
         // const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-
         // // Listen for changes to the prefers-color-scheme media query
         // prefersDark.addListener((mediaQuery) =>
         //     this.toggleDarkMode(mediaQuery.matches)
         // );
-
-        const darkMode = localStorage.getItem('darkmode');
-
-        if (darkMode && darkMode == 'true') {
-            this.toggleDarkMode(true);
-        } else if (darkMode && darkMode == 'false') {
-            this.toggleDarkMode(false);
-        }
+        // const darkMode = localStorage.getItem('darkmode');
+        // if (darkMode && darkMode == 'true') {
+        //     this.toggleDarkMode(true);
+        // } else if (darkMode && darkMode == 'false') {
+        //     this.toggleDarkMode(false);
+        // }
     }
 
-    toggleDarkMode(shouldAdd) {
-        console.log('Should -> ', shouldAdd);
-        this.darkMode = shouldAdd;
-        localStorage.setItem('darkmode', shouldAdd);
-        if (shouldAdd) {
-            document.body.classList.toggle('dark', true);
-            document.body.classList.toggle('light', false);
-        } else {
-            document.body.classList.toggle('light', true);
-            document.body.classList.toggle('dark', false);
-        }
-    }
+    // toggleDarkMode(shouldAdd) {
+    //     console.log('Should -> ', shouldAdd);
+    //     this.darkMode = shouldAdd;
+    //     localStorage.setItem('darkmode', shouldAdd);
+    //     if (shouldAdd) {
+    //         document.body.classList.toggle('dark', true);
+    //         document.body.classList.toggle('light', false);
+    //     } else {
+    //         document.body.classList.toggle('light', true);
+    //         document.body.classList.toggle('dark', false);
+    //     }
+    // }
 
     ToggleNavBar() {
         let element: HTMLElement = document.getElementsByClassName(
