51bc987be4786e5c337455bcc1722f9b72c1b546
Vadim <vadimpishprivate@gmail.com>
2021-08-30 17:59:30 -0700

    debugging window bug on SSR

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
 import {
     faChevronRight,
     faEnvelopeOpen,
     faSun,
     faMoon,
 } from '@fortawesome/free-solid-svg-icons';
+import { isPlatformBrowser } from '@angular/common';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
@@ -18,38 +19,44 @@ export class HeaderComponent implements OnInit {
     isCollapsed: boolean = false;
     darkMode: boolean = false;
 
-    constructor() {
+    constructor(@Inject(PLATFORM_ID) private platformId: any) {
         this.darkModeInit();
     }
 
     ngOnInit(): void {}
 
     darkModeInit() {
-        // const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-        // // Listen for changes to the prefers-color-scheme media query
-        // prefersDark.addListener((mediaQuery) =>
-        //     this.toggleDarkMode(mediaQuery.matches)
-        // );
-        // const darkMode = localStorage.getItem('darkmode');
-        // if (darkMode && darkMode == 'true') {
-        //     this.toggleDarkMode(true);
-        // } else if (darkMode && darkMode == 'false') {
-        //     this.toggleDarkMode(false);
-        // }
+        if (isPlatformBrowser(this.platformId)) {
+            const prefersDark = window.matchMedia(
+                '(prefers-color-scheme: dark)'
+            );
+            // Listen for changes to the prefers-color-scheme media query
+            prefersDark.addListener((mediaQuery) =>
+                this.toggleDarkMode(mediaQuery.matches)
+            );
+            const darkMode = localStorage.getItem('darkmode');
+            if (darkMode && darkMode == 'true') {
+                this.toggleDarkMode(true);
+            } else if (darkMode && darkMode == 'false') {
+                this.toggleDarkMode(false);
+            }
+        }
     }
 
-    // toggleDarkMode(shouldAdd) {
-    //     console.log('Should -> ', shouldAdd);
-    //     this.darkMode = shouldAdd;
-    //     localStorage.setItem('darkmode', shouldAdd);
-    //     if (shouldAdd) {
-    //         document.body.classList.toggle('dark', true);
-    //         document.body.classList.toggle('light', false);
-    //     } else {
-    //         document.body.classList.toggle('light', true);
-    //         document.body.classList.toggle('dark', false);
-    //     }
-    // }
+    toggleDarkMode(shouldAdd) {
+        if (isPlatformBrowser(this.platformId)) {
+            console.log('Should -> ', shouldAdd);
+            this.darkMode = shouldAdd;
+            localStorage.setItem('darkmode', shouldAdd);
+            if (shouldAdd) {
+                document.body.classList.toggle('dark', true);
+                document.body.classList.toggle('light', false);
+            } else {
+                document.body.classList.toggle('light', true);
+                document.body.classList.toggle('dark', false);
+            }
+        }
+    }
 
     ToggleNavBar() {
         let element: HTMLElement = document.getElementsByClassName(
